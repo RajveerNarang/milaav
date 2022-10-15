f1223c1c051cb955892fbdb8173bf5e37ddef3aa
RajveerNarang <dhanurrajveer@gmail.com>
2022-10-15 14:07:13 +0530

    base structure

---
+++ client/src/components/CallPage/CallPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CallPage.scss";
+
+const CallPage = () => {
+  return <div>CallPage</div>;
+};
+
+export default CallPage;
---
+++ client/src/components/NoMatch/NoMatch.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./NoMatch.scss";
+
+const NoMatch = () => {
+  return <div>NoMatch</div>;
+};
+
+export default NoMatch;
