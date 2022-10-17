2ace4398e280f399596bd7e79ab38b7251aa1b47
RajveerNarang <dhanurrajveer@gmail.com>
2022-10-17 15:11:21 +0530

    NoMatch Page Completed

--- client/src/components/NoMatch/NoMatch.js
+++ client/src/components/NoMatch/NoMatch.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./NoMatch.scss";
+import Header from "../UI/Header/Header";
+import { Link } from "react-router-dom";
 
 const NoMatch = () => {
   return (
     <>
-      <h1>No Match</h1>
+      {/* <h1>No Match</h1> */}
+      <div className="no-match">
+        <Header />
+        <div className="no-match__content">
+          <h2>Invalid Meeting ID.</h2>
+          <div className="action-btn">
+            <Link className="btn green" to="/">
+              Return To HomeScreen
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
