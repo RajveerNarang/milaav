cb9caeae99359ded881db178ad21db7ce827410c
RajveerNarang <dhanurrajveer@gmail.com>
2022-10-20 00:19:15 +0530

    Meeting Info Bar Admin Side Only

--- client/src/components/CallPage/CallPage.js
+++ client/src/components/CallPage/CallPage.js
@@ -1,11 +1,25 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import "./CallPage.scss";
+import { useParams, useNavigate } from "react-router-dom";
 import CallPageHeader from "../UI/CallPageHeader/CallPageHeader";
 import CallPageFooter from "../UI/CallPageFooter/CallPageFooter";
 import MeetingInfo from "../UI/MeetingInfo/MeetingInfo";
 import Messenger from "../UI/Messenger/Messenger";
 
 const CallPage = () => {
+  let { id } = useParams();
+  const isAdmin = window.location.hash == "#init" ? true : false;
+  const url = `${window.location.origin}${window.location.pathname}`;
+  // let alertTimeout = null;
+  // console.log(isAdmin, url);
+  const [meetInfoPopup, setMeetInfoPopup] = useState(false);
+
+  useEffect(() => {
+    if (isAdmin) {
+      setMeetInfoPopup(true);
+    }
+  }, []);
+
   return (
     <>
       {/* <h1>Call Page</h1> */}
@@ -13,7 +27,10 @@ const CallPage = () => {
         <video className="video-container" src="" controls></video>
         <CallPageHeader />
         <CallPageFooter />
-        <MeetingInfo />
+        {/* <MeetingInfo /> */}
+        {isAdmin && meetInfoPopup && (
+          <MeetingInfo setMeetInfoPopup={setMeetInfoPopup} url={url} />
+        )}
         <Messenger />
       </div>
     </>
